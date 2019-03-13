c9eed4f68aba68c4c5f70e05ad76e59eb7396ac1
JbareenM <44744884+JbareenM@users.noreply.github.com>
2019-03-13 00:20:11 -0700

    Add files via upload

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -14,8 +14,7 @@ if [[ -e $Path ]]; then
 		if [[ $? -gt 0 ]]; then
 			Memory="FAIL"
 			valgrind --tool=helgrind ./$Program 1> /dev/null 2> /dev/null
-			CHECK=$?
-			if [[ $CHECK -gt 0 ]]; then 
+			if [[ $? -gt 0 ]]; then 
 				thread="FAIl"
 			fi
 		else
@@ -24,21 +23,20 @@ if [[ -e $Path ]]; then
 				thread="FAIL"
 			fi
 		fi
+        	echo "Compilation        Memory leaks            thread race"
+        	echo "   $compilation                $Memory                    $thread"
+        	if [[ "$compilation" == "PASS" && "$Memory" == "PASS" && "$thread" == "PASS" ]]; then
+   			exit 0
+        	elif [[ "$compilation" == "PASS" && "$Memory" == "FAIL" && "$thread" == "PASS" ]]; then
+       			exit 2
+       		elif [[ "$compilation" == "PASS" && "$Memory" == "PASS" && "$thread" == "FAIL" ]]; then
+        	        exit 1
+       		elif [[ "$compilation" == "PASS" && "$Memory" == "FAIL" && "$thread" == "FAIL" ]]; then
+        	        exit 3
+       		else
+        	   	exit 7
+		fi
 	fi
-        echo "Compilation        Memory leaks            thread race"
-        echo "   $compilation                $Memory                    $thread"
-        if [[ "$compilation" == "PASS" && "$Memory" == "PASS" && "$thread" == "PASS" ]]; then
-   		exit 0
-        elif [[ "$compilation" == "PASS" && "$Memory" == "FAIL" && "$thread" == "PASS" ]]; then
-       		exit 2
-        elif [[ "$compilation" == "PASS" && "$Memory" == "PASS" && "$thread" == "FAIL" ]]; then
-                exit 1
-        elif [[ "$compilation" == "PASS" && "$Memory" == "FAIL" && "$thread" == "FAIL" ]]; then
-                exit 3
-        else
-                exit 7
-	fi
-
 else
 	echo path not exists
 	exit 7

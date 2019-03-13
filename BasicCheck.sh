77ed692b56442bf1acd88f4ba3f5d987cec8b41f
JbareenM <44744884+JbareenM@users.noreply.github.com>
2019-03-13 02:26:25 -0700

    Add files via upload

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -21,18 +21,24 @@ if [[ -e $Path ]]; then
 			valgrind --error-exitcode=1 --tool=helgrind ./$Program 1> /dev/null 2> /dev/null
 			if [[ $? -gt 0 ]]; then
 				Thr=1	
-			if
+			fi
 		fi
 	fi
 	echo "Compilation     Memory leak     Thread race"
 	echo "   $Comp		 $Mem		 $Thr"
-if [[ $Comp == 1 ]];then exit 7 fi
-if [[ $Mem == 1 ]];then
-	if [[ $Thr == 1 ]];then exit 3 
-	else exit 2
+	if [[ $Comp == 1 ]];then 
+		exit 7
+       	fi
+	if [[ $Mem == 1 ]];then
+		if [[ $Thr == 1 ]];then
+		       	exit 3 
+		else
+	       		exit 2
+		fi
 	fi
-fi
-if [[ $Thr == 1 ]];then exit 1
-else exit 0
-fi
+	if [[ $Thr == 1 ]];then
+	       	exit 1
+	else
+	       	exit 0
+	fi
 fi

41af914022e0bdd18773a8eb24f87679fc6b3a2b
JbareenM <44744884+JbareenM@users.noreply.github.com>
2019-03-13 01:09:20 -0700

    Add files via upload

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 Path=$1
 Program=$2
-Comp=0
-Mem=0
-Thr=0
 if [[ -e $Path ]]; then
 	cd $Path
-	make > /dev/null
-	if [[ $? -gt 0 ]]; then exit 7
+	make 1> /dev/null 2> /dev/null
+	if [[ $? -gt 0 ]]; then 
+		echo 7       
+		exit 7
 	else
 		valgrind --leak-check=full ./$Program 1> /dev/null 2> /dev/null
 		if [[ $? -gt 0 ]]; then 
 			valgrind --tool=helgrind ./$Program 1> /dev/null 2> /dev/null
-			if [[ $? -gt 0 ]]; then exit 3
-			else exit 2
+			if [[ $? -gt 0 ]]; then
+				echo 3       
+				exit 3
+			else
+				echo 2       
+				exit 2
 			fi
 		else
 			valgrind --tool=helgrind ./$Program 1> /dev/null 2> /dev/null
-			if [[ $? -gt 0 ]]; then exit 1
-			else exit 0
+			if [[ $? -gt 0 ]]; then
+				echo 1	
+				exit 1
+			else 
+				echo 0
+				exit 0
 			fi
 		fi
 	fi
